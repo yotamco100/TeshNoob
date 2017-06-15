a071361ce3059bd78bd0657ed2beb070a6d1c042
yotamco100 <yotxbox@hotmail.com>
2017-06-15 23:33:32 +0300

    Create SexDiceGen.py
    more options

--- SexDiceGen.py
+++ SexDiceGen.py
@@ -3,7 +3,7 @@ import random
 
 dice1=['whisper into','fuck','suck','tickle','smother','tie up','handjob','threesome with a nigger and','cuddle','titfuck','send tentacles on']
 dice2=['Tzach','Bush','Avrech', 'Nitay', 'Byle', 'Tzabari', 'Shrek', 'Nave', 'Ofek','Yotam','Kalish']
-dice3=['','with clothes on', 'in the fridge', '\'s asshole', 'in the gorilla cage', 'at Dizengoff Center', 'while 9/11 happens', 'at school', 'but it\'s a hentai']
+dice3=['','with clothes on', 'in the fridge', '\'s asshole', 'in the gorilla cage', 'at Dizengoff Center', 'while 9/11 happens', 'at school', 'but it\'s a hentai', 'while locked in your basement', 'on Shrek\'s dick', 'in a sick minecraft server', 'while jacking Zeus off']
 
 def randomchoose(arr):
     return arr[random.randint(0, len(arr)-1)]
